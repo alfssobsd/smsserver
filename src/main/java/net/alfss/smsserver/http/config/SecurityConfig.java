3b87b8ea284d6b07b68e5a93bbcb9992703bab9f
Sergey V. Kravchuk <alfss.obsd@gmail.com>
2013-10-29 11:51:55 +0400

    [~] fix build [+] add spring security

---
+++ src/main/java/net/alfss/smsserver/http/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package net.alfss.smsserver.http.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * User: alfss
+ * Date: 29.10.13
+ * Time: 10:41
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
