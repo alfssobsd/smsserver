df6acbec71a53a3eb0ad4d947b09bbf50907bc57
Sergey V. Kravchuk <alfss.obsd@gmail.com>
2013-10-29 11:50:45 +0400

    [-] delete not used field

--- src/main/java/net/alfss/smsserver/http/web/Home.java
+++ src/main/java/net/alfss/smsserver/http/web/Home.java
@@ -6,23 +6,13 @@ package net.alfss.smsserver.http.web;
  * Time: 13:11
  */
 
-import net.alfss.smsserver.http.services.ChannelServices;
-import net.alfss.smsserver.http.services.UserServices;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Home {
-    @Autowired
-    private ChannelServices channelServices;
-    @Autowired
-    private UserServices userServices;
-
-
     @RequestMapping("/")
-    public String home(Model model)
+    public String home()
     {
         return "index";
     }
